e630d8361589397c647add5e9f5b50d2b417a40a
StevenZambito <zambito.steven@icloud.com>
2021-01-05 13:48:54 -0500

    Practice Creating Variables completed

--- Program.cs
+++ Program.cs
@@ -2,11 +2,21 @@
 
 namespace VariablesCS2
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-      Console.WriteLine("Welcome to C#");
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to C#");
+
+            var numberOfCupsOfCoffee = 0;
+            var fullName = "Steven Zambito";
+            var today = "January 5th, 2021";
+
+            Console.WriteLine(numberOfCupsOfCoffee);
+            Console.WriteLine(fullName);
+            Console.WriteLine(today);
+
+
+        }
     }
-  }
 }
