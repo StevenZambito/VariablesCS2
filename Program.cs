ef730a41af951830b304d4b333208ad15e2b1f97
StevenZambito <zambito.steven@icloud.com>
2021-01-05 16:40:53 -0500

    Finished all tasks

--- Program.cs
+++ Program.cs
@@ -10,23 +10,43 @@ namespace VariablesCS2
 
             var numberOfCupsOfCoffee = 0;
             var fullName = "Steven Zambito";
-            var today = "January 5th, 2021";
+            var today = new DateTime();
 
             Console.WriteLine(numberOfCupsOfCoffee);
             Console.WriteLine(fullName);
             Console.WriteLine(today);
 
+            Console.WriteLine(" ");
+
             Console.Write("What is your name? ");
             var userName = Console.ReadLine();
-            Console.WriteLine($"Nice to meet you {userName}!");
+            if (userName == "Alice")
+            {
+                Console.WriteLine("Hello Alice!");
+            }
+            else
+            {
+                Console.WriteLine($"Nice to meet you {userName}!");
+            }
+
+            Console.WriteLine(" ");
 
             Console.WriteLine("Please input a number here:");
-            var firstNumberAsString = int.Parse(Console.ReadLine());
+            var firstNumberAsString = Console.ReadLine();
             Console.WriteLine("Please input another number here:");
-            var secondNumberAsString = int.Parse(Console.ReadLine());
+            var secondNumberAsString = Console.ReadLine();
 
+            var firstOperand = double.Parse(firstNumberAsString);
+            var secondOperand = double.Parse(secondNumberAsString);
 
+            var sum = firstOperand + secondOperand;
+            var difference = (firstOperand - secondOperand);
+            var product = (firstOperand * secondOperand);
+            var quotient = (firstOperand / secondOperand);
+            var remainder = (firstOperand % secondOperand);
 
+            Console.WriteLine(" ");
+            Console.WriteLine($"If you add {firstNumberAsString} and {secondNumberAsString}, you get {sum}. {firstOperand} minus {secondOperand} is {difference}. {firstOperand} divided by {secondOperand} is {quotient}. The product of {firstOperand} and {secondOperand} is {product}. The remainder of {firstOperand} divided by {secondOperand} is {remainder}.");
         }
     }
 }
